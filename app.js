15863ac9752d109149ab3df4fd2743338cf7bf1e
ivan emmanuel gomez <ivanemmanuelgomez2017@gmail.com>
2026-08-06 09:44:30 -0300

    feat(app.js): se conecto la base de datos con el servidor

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 import express from "express"
+import { DataBaseUp } from "./src/config/DataBase.js";
 
 const app = express();
 app.use(express.json());
 
 let port = 3000;
 
-app.listen(port, ()=>{
-    console.log(`se ensendio el server correctamente en el puerto ${port}`)
-})
+app.listen(port, async ()=>{
+    try {
+        await DataBaseUp()
+        console.log(`se ensendio el server correctamente en el puerto ${port}`)
+    } catch (error) {
+        console.log(`lo siento ocurrio un error en levantar el server :(`);
+    }
+});
 

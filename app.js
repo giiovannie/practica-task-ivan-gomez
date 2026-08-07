c38fb5c628acc7b51874bb7fb4eba959df489691
Iván Emmanuel Gómez <ivanemmanuelgomez2017@gmail.com>
2026-08-07 14:57:22 -0300

    feat: se instalo la dependencia de dotenv y se importo al proyecto para usar las variables de entorno

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import { DataBaseUp } from "./src/config/DataBase.js";
 import { UserRoutes } from "./src/routes/User.route.js";
 import { TaskRouter } from "./src/routes/Task.routes.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
@@ -10,7 +13,7 @@ app.use("/api/users", UserRoutes)
 app.use("/api/tasks", TaskRouter)
 
 
-let port = 3000;
+let port = process.env.PORT;
 
 app.listen(port, async ()=>{
     try {

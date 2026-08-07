36eda30611956e9cd003a2a8783bb80480048c34
Iván Emmanuel Gómez <ivanemmanuelgomez2017@gmail.com>
2026-08-07 13:54:53 -0300

    feat: se agregaron dos endpoints que permiten borrar o actualizar una tarea

--- src/controllers/Task.Controller.js
+++ src/controllers/Task.Controller.js
@@ -62,3 +62,41 @@ export const mostrarTarea = async (req,res) => {
         return res.status(500).json(MESSAGES[500])
     }
 }
+
+export const actualizarTarea = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const tareaEncontrada = await TableTask.findOne({ where: { id } });
+
+        if (!tareaEncontrada) return res.status(404).json(MESSAGES[404]);
+
+        const { title, description, isComplete } = req.body;
+
+        if (!title || !description) return res.status(400).json(MESSAGES[400]);
+        if (title.length > 100 || description.length > 100) return res.status(400).json(MESSAGES[400]);
+        if (typeof isComplete !== "boolean") return res.status(400).json(MESSAGES[400]);
+
+        await TableTask.update({ title, description, isComplete }, { where: { id } });
+
+        const tareaActualizada = await TableTask.findOne({ where: { id } });
+        return res.status(200).json(tareaActualizada);
+
+    } catch (error) {
+        return res.status(500).json(MESSAGES[500]);
+    }
+};
+
+export const eliminarTarea = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const tareaEncontrada = await TableTask.findOne({ where: { id } });
+
+        if (!tareaEncontrada) return res.status(404).json(MESSAGES[404]);
+
+        await tareaEncontrada.destroy();
+        return res.status(200).json({ message: "Tarea eliminada exitosamente." });
+
+    } catch (error) {
+        return res.status(500).json(MESSAGES[500]);
+    }
+};
--- src/routes/Task.routes.js
+++ src/routes/Task.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { agreguaTarea, mostrarTarea, mostrarTareas } from "../controllers/Task.Controller.js";
+import { actualizarTarea, agreguaTarea, eliminarTarea, mostrarTarea, mostrarTareas } from "../controllers/Task.Controller.js";
 
 export const TaskRouter = Router();
 
 TaskRouter.post("/", agreguaTarea);
 TaskRouter.get("/", mostrarTareas);
-TaskRouter.get("/:id", mostrarTarea)
+TaskRouter.get("/:id", mostrarTarea);
+TaskRouter.put("/:id", actualizarTarea);
+TaskRouter.delete("/:id", eliminarTarea);
